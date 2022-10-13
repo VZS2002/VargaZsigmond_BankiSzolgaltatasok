989fbdcdec25dbc51a3f36ed284ad66f265770c1
Varga Zsigmond <petrikdiak@gmail.com>
2022-10-13 09:06:12 +0200

    Tulajdonos

---
+++ src/hu/vargazsigmond/bankiszolgaltatasok/Tulajdonos.java
@@ -0,0 +1,17 @@
+package hu.vargazsigmond.bankiszolgaltatasok;
+
+public final class Tulajdonos {
+    private String nev;
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public Tulajdonos(String nev) {
+        this.nev = nev;
+    }
+}
