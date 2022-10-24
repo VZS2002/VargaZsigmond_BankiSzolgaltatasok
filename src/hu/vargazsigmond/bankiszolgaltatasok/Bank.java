61372a9cf2d6ba27d0e85687e2ec880da5691382
Varga <vargazsigmond02@gmail.com>
2022-10-24 17:10:57 +0200

    .

--- src/hu/vargazsigmond/bankiszolgaltatasok/Bank.java
+++ src/hu/vargazsigmond/bankiszolgaltatasok/Bank.java
@@ -5,34 +5,31 @@ import java.util.List;
 
 public class Bank {
 
-    private List<Szamla> szamaLista = new ArrayList<>();
+    private List<Szamla> szamlaLista = new ArrayList<>();
 
     public Bank() {
 
     }
-}
-/*
-    public Szamla szamlanyitas(Tulajdonos tulajdonos, int HitelKeret){
-        if (HitelKeret == 0)
-        {
+    public Szamla szamlanyitas(Tulajdonos tulajdonos, int HitelKeret) {
+        if (HitelKeret == 0) {
             MegtakaritasSzamla megtakaritasi = new MegtakaritasSzamla(tulajdonos);
-            szamaLista.add(megtakaritasi);
+            szamlaLista.add(megtakaritasi);
             return megtakaritasi;
-        }
-        else {
-           HitelSzamla hitelSzamla = new HitelSzamla(tulajdonos, HitelKeret);
-           return hitelszamla;
+        } else {
+            HitelSzamla hitelszamla = new HitelSzamla(tulajdonos, HitelKeret);
+            szamlaLista.add(hitelszamla);
+            return hitelszamla;
+
         }
     }
-    public int getOsszegyenleg(Tulajdonos tulajdonos){
-        int osszeg=0;
-        for (Szamla sz : szamaLista){
-            if (sz.getTulajdonos()==tulajdonos)
-            {
-                osszeg+=sz.getAktualisEgyenleg();
+
+    public int getOsszegyenleg(Tulajdonos tulajdonos) {
+        int osszeg = 0;
+        for (Szamla sz : szamlaLista) {
+            if (sz.getTulajdonos() == tulajdonos) {
+                osszeg += sz.getAktualisEgyenleg();
             }
         }
         return osszeg;
     }
 }
-*/
--- src/hu/vargazsigmond/bankiszolgaltatasok/MegtakaritasSzamla.java
+++ src/hu/vargazsigmond/bankiszolgaltatasok/MegtakaritasSzamla.java
@@ -1,4 +1,37 @@
 package hu.vargazsigmond.bankiszolgaltatasok;
 
-public class MegtakaritasSzamla {
+public class MegtakaritasSzamla extends Szamla {
+    private double kamat;
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    public static double alapKamat =10;
+
+    public MegtakaritasSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        this.kamat = alapKamat;
+    }
+
+
+    public boolean kivesz(int osszeg) {
+        if (super.getAktualisEgyenleg() - osszeg >0) {
+            super.aktualisEgyenleg -=osszeg;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+    public void kamatJovairas() {
+        super.aktualisEgyenleg =(int) (aktualisEgyenleg*(1+kamat/100));
+    }
+
+
 }
